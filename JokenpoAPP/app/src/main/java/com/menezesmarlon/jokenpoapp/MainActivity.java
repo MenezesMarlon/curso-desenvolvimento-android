9cb0159a292f51d65795c0b5556a2123fce453d8
MenezesMarlon <80010030+MenezesMarlon@users.noreply.github.com>
2023-04-28 18:26:11 -0300

    Criação dos metodos onClick

--- JokenpoAPP/app/src/main/java/com/menezesmarlon/jokenpoapp/MainActivity.java
+++ JokenpoAPP/app/src/main/java/com/menezesmarlon/jokenpoapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.menezesmarlon.jokenpoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void opcaoPedra(View view) {
+        this.opcaoEscolhida("Pedra");
+    }
+
+    public void opcaoPapel(View view) {
+        this.opcaoEscolhida("Papel");
+    }
+
+    public void opcaoTesoura(View view) {
+        this.opcaoEscolhida("Tesoura");
+    }
+
+    public void opcaoEscolhida(String opcao) {
+        TextView textOpcaoEscolhida = findViewById(R.id.textOpcaoEscolhida);
+        textOpcaoEscolhida.setText("Você Escolheu: " + opcao);
     }
 }
