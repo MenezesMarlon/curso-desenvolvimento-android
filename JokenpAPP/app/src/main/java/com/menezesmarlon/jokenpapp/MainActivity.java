33eeafe6bbcd6514b6dad60519af6a99f53e4f09
MenezesMarlon <80010030+MenezesMarlon@users.noreply.github.com>
2023-04-28 17:41:43 -0300

    Criação dos metodos para evento onClick

--- JokenpAPP/app/src/main/java/com/menezesmarlon/jokenpapp/MainActivity.java
+++ JokenpAPP/app/src/main/java/com/menezesmarlon/jokenpapp/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,4 +15,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
+
+    public void selecionadoPedra(View view) {
+        this.opcaoSelecionada("Pedra");
+    }
+
+    public void selecionadoPapel(View view) {
+        this.opcaoSelecionada("Papel");
+    }
+
+    public void selecionadoTesoura(View view) {
+        this.opcaoSelecionada("Tesoura");
+    }
+
+    public void opcaoSelecionada(String opcao){
+        TextView textOpcaoEscolhida = findViewById(R.id.textOpcaoEscolhida);
+        textOpcaoEscolhida.setText("Você Escolheu: " + opcao);
+    }
+
+
+
 }
