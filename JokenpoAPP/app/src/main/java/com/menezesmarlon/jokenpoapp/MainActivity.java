cb9a41a8442db24cf851695a685ac5e481d41692
MenezesMarlon <80010030+MenezesMarlon@users.noreply.github.com>
2023-04-30 11:26:24 -0300

    Criação do método para escolha da máquina

--- JokenpoAPP/app/src/main/java/com/menezesmarlon/jokenpoapp/MainActivity.java
+++ JokenpoAPP/app/src/main/java/com/menezesmarlon/jokenpoapp/MainActivity.java
@@ -1,10 +1,16 @@
 package com.menezesmarlon.jokenpoapp;
 
+import androidx.annotation.DrawableRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,5 +35,26 @@ public class MainActivity extends AppCompatActivity {
     public void opcaoEscolhida(String opcao) {
         TextView textOpcaoEscolhida = findViewById(R.id.textOpcaoEscolhida);
         textOpcaoEscolhida.setText("Você Escolheu: " + opcao);
+        ImageView imagePadrao = findViewById(R.id.imagePadrao);
+
+        int numero = new Random().nextInt(3);
+        String[] opcoes = {"Pedra", "Papel", "Tesoura"};
+        String opcaoApp = opcoes[numero];
+
+        switch (opcaoApp) {
+            case "Pedra":
+                imagePadrao.setImageResource(R.drawable.pedra);
+
+                break;
+            case "Papel":
+                imagePadrao.setImageResource(R.drawable.papel);
+
+                break;
+            case "Tesoura":
+                imagePadrao.setImageResource(R.drawable.tesoura);
+
+                break;
+        }
+
     }
 }
