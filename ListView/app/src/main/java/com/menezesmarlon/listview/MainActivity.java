fe5421f501e22ee6b01b6b2d3f2aa53b0842a2ba
MenezesMarlon <80010030+MenezesMarlon@users.noreply.github.com>
2023-05-07 10:59:39 -0300

    Criação do Array com os itens do List View

--- ListView/app/src/main/java/com/menezesmarlon/listview/MainActivity.java
+++ ListView/app/src/main/java/com/menezesmarlon/listview/MainActivity.java
@@ -3,12 +3,23 @@ package com.menezesmarlon.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listLocais;
+    private String[] itens = {
+            "Angra dos Reis", "Caldas Novas",
+            "Campos do Jordão", "Costa do Sauípe",
+            "Ilhéus", "Porto Seguro", "Tiradentes",
+            "Praga", "Santiago", "Zurique", "Caribe",
+            "Buenos Aires", "Budapeste", "Cancún", "Aruba",
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
