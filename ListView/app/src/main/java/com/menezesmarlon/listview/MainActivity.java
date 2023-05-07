007af88d9ef65458f0b1665c3bc5839377c86f98
MenezesMarlon <80010030+MenezesMarlon@users.noreply.github.com>
2023-05-07 11:18:47 -0300

    Criação do Método de Listagem do Locais

--- ListView/app/src/main/java/com/menezesmarlon/listview/MainActivity.java
+++ ListView/app/src/main/java/com/menezesmarlon/listview/MainActivity.java
@@ -3,6 +3,7 @@ package com.menezesmarlon.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +21,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listarLocais();
+
+    }
+
+    public void listarLocais(){
+        listLocais = findViewById(R.id.listLocais);
+
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                itens
+        );
+
+        listLocais.setAdapter(adaptador);
     }
 
 }
