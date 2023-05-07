b9d9f1ba5b2f10d3f44e2bebefb32c9f49ec8375
MenezesMarlon <80010030+MenezesMarlon@users.noreply.github.com>
2023-05-07 11:24:48 -0300

    criação do Toast para retornar os locais clicados

--- ListView/app/src/main/java/com/menezesmarlon/listview/MainActivity.java
+++ ListView/app/src/main/java/com/menezesmarlon/listview/MainActivity.java
@@ -3,8 +3,11 @@ package com.menezesmarlon.listview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,12 +24,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         listarLocais();
 
     }
 
-    public void listarLocais(){
+    public void listarLocais() {
         listLocais = findViewById(R.id.listLocais);
 
         ArrayAdapter<String> adaptador = new ArrayAdapter<String>(
@@ -35,8 +37,15 @@ public class MainActivity extends AppCompatActivity {
                 android.R.id.text1,
                 itens
         );
-
         listLocais.setAdapter(adaptador);
+
+        listLocais.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String valueSelected = listLocais.getItemAtPosition(position).toString();
+                Toast.makeText(MainActivity.this, valueSelected, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
